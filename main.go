4577bb45fa7a279512789cc10a97400c412ee681
baoqiang <mailbaoqiang@gmail.com>
2018-07-11 17:39:39 +0800

    go lang test
    Change-Id: Ie5f6b3d9447cfee89a37482f5c031f015b09f2fc

--- main.go
+++ main.go
@@ -3,10 +3,13 @@
 // time: 2018/6/3 下午4:56
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main()  {
-	sli := make([] int,1,3)
+func main1() {
+	sli := make([]int, 1, 3)
 
 	//sli[0] = 2
 	sli[1] = 2
@@ -14,3 +17,8 @@ func main()  {
 	fmt.Print(sli)
 }
 
+func main() {
+	current := time.Now().Format("2006-01-02 15:04:05")
+	text := fmt.Sprintf("i recieved %s at %s", "aaa", current)
+	fmt.Print(text)
+}
